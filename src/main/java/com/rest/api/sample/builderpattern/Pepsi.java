64fb4977c6ac7d7eab984523954d5f0fe4ff8dbe
gumpenavarun <varunchakravarthy27@gmail.com>
2021-03-24 03:27:03 -0500

    builder pattern

---
+++ src/main/java/com/rest/api/sample/builderpattern/Pepsi.java
@@ -0,0 +1,18 @@
+package com.rest.api.sample.builderpattern;
+
+public class Pepsi extends CoolDrink {
+
+	
+	@Override
+	public String name() { // TODO Auto-generated method stub return
+		return "Pepsi";
+	}
+	 
+
+	@Override
+	public float price() {
+		// TODO Auto-generated method stub
+		return 35.0f;
+	}
+
+}
---
+++ src/main/java/com/rest/api/sample/builderpattern/Wrapper.java
@@ -0,0 +1,10 @@
+package com.rest.api.sample.builderpattern;
+
+public class Wrapper implements Packing{
+
+	@Override
+	public String pack() {
+		return "Wrapper";
+	}
+
+}
