085becc257c9be70385047c1d7d67bacc331643c
gumpenavarun <varunchakravarthy27@gmail.com>
2021-03-24 03:23:22 -0500

    Adding Builder Patter in

--- src/com/rest/api/sample/controller/Check.java
+++ src/com/rest/api/sample/controller/Check.java
@@ -1,15 +1,11 @@
 package com.rest.api.sample.controller;
 
-import java.util.List;
-
-import com.rest.api.sample.controller.SampleController;
-import com.rest.api.sample.pojo.Student;
-
 public class Check {
 	public static void main(String[] args) {
 		Shape s1 = new Circle("Red", 2.2);
         //Shape s2 = new Rectangle("Yellow", 2, 4);
         System.out.println(s1.toString());
-        //System.out.println(s2.toString());
+        //System.out.println(s2.toString()); 
+        
 	}
 }
